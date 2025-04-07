8bd76ac7799514a132bcd3e4e2abce11ff1e992a
Joseph Axisa <josephaxisa@gmail.com>
2025-04-07 17:33:13 +0000

    feat: Add tooltips to OneVizBarChart

--- src/components/BarChart/OneVizBarChart.ts
+++ src/components/BarChart/OneVizBarChart.ts
@@ -1,34 +1,65 @@
+import { LitElement, html, css } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import * as Highcharts from 'highcharts';
-import { AbstractChart, ChartData } from './AbstractChart';
-import { html, css } from 'lit';
 
 @customElement('oneviz-barchart')
-export class OneVizBarChart extends AbstractChart {
+export class OneVizBarChart extends LitElement {
 
-  static styles = [
-    AbstractChart.styles,
-    css`
-      :host {
-        --oneviz-bar-color: #4CAF50;
-      }
-    `
-  ];
-    @property({ type: String }) override title: string = 'OneViz Bar Chart';
+  @property({ type: String, attribute: 'data-url' }) dataUrl = '';
+  @property({ type: String, attribute: 'x-field' }) xField = '';
+  @property({ type: String, attribute: 'y-field' }) yField = '';
+  @property({ type: String }) title = 'OneViz Bar Chart';
+  @property({ type: Array }) data: Array<any> = [];
+
+  static styles = css`
+    :host {
+      display: block;
+      width: 100%;
+      height: 400px; /* Or any default height */
+    }
+    #chart {
+      width: 100%;
+      height: 100%;
+    }
+  `;
+
+  private chart: Highcharts.Chart | undefined;
+
+  async firstUpdated() {
+    if (this.dataUrl) {
+      await this.fetchData();
+    }
+    this.createChart();
+  }
+
+  updated(changedProperties: Map<string | number | symbol, unknown>) {
+    if (changedProperties.has('data') || changedProperties.has('xField') || changedProperties.has('yField')) {
+      this.createChart();
+    }
+  }
+
+  async fetchData() {
+    try {
+      const response = await fetch(this.dataUrl);
+      this.data = await response.json();
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
 
   createChart() {
     if (!this.data || this.data.length === 0 || !this.xField || !this.yField) {
-      return;
+      return; // Don't create a chart if data or fields are missing
     }
 
-    const categories = this.data.map((item) => String(item[this.xField]));
-    const seriesData = this.data.map((item) => Number(item[this.yField]));
+    const categories = this.data.map((item) => item[this.xField]);
+    const seriesData = this.data.map((item) => item[this.yField]);
 
     if (this.chart) {
-        this.chart.destroy();
+      this.chart.destroy();
     }
 
-    this.chart = Highcharts.chart(this.shadowRoot!.getElementById('chart')!, {
+    this.chart = Highcharts.chart(this.shadowRoot!.getElementById('chart')!, { // Ensure shadowRoot is not null
       chart: {
         type: 'bar'
       },
@@ -44,36 +75,22 @@ export class OneVizBarChart extends AbstractChart {
       yAxis: {
         title: {
           text: this.yField
-        },
-        min: 0,
+        }
+      },
+      tooltip: {
+        formatter: function() {
+          return `<b>${this.x}</b><br/>${this.series.name}: ${this.y}`; 
+        }
       },
       series: [{
         type: 'bar',
         name: this.yField,
-        data: seriesData,
-        color: 'var(--oneviz-bar-color)',
-        point: { // Add the event handler here
-          events: {
-            click: (event: Highcharts.PointClickEventObject) => {
-              this.dispatchEvent(new CustomEvent('oneviz-bar-click', {
-                detail: {
-                  x: event.point.category, // x-axis value (category)
-                  y: event.point.y,        // y-axis value
-                  originalEvent: event     // The original Highcharts event
-                },
-                bubbles: true,   // Allow the event to bubble up the DOM
-                composed: true  // Allow the event to cross the shadow DOM boundary
-              }));
-            }
-          }
-        }
-      }],
-      credits: {
-        enabled: false
-      },
-      tooltip: {
-          pointFormat: '<b>{point.y}</b><br/>{point.category}',
-      }
+        data: seriesData
+      }]
     });
+  }
+
+  render() {
+    return html`<div id="chart"></div>`;
   }
 }
