7c68e51b73d2ea645d98afe69bfe99d4b3b8c966
Joseph Axisa <josephaxisa@google.com>
2025-08-11 17:16:20 +0000

    feat: Add Scatter Chart component
    - Created the `oneviz-scatterchart` component, completing the core set of basic visualizations.
    - Implemented a click event handler (`oneviz-scatter-click`) for the new component.
    - Updated the demo page to include a scatter chart example and to log its events.

---
+++ src/components/ScatterChart/OneVizScatterChart.ts
@@ -0,0 +1,58 @@
+import { customElement } from 'lit/decorators.js';
+import type { Options } from 'highcharts';
+import { AbstractChart } from '../AbstractChart/AbstractChart';
+
+@customElement('oneviz-scatterchart')
+export class OneVizScatterChart extends AbstractChart {
+  
+  getChartOptions(): Options | null {
+    if (!this.data || this.data.length === 0) {
+        return null;
+    }
+
+    const seriesData = this.data.map((item: any) => ({
+        x: item[this.xField],
+        y: item[this.yField]
+    }));
+
+    return {
+      chart: {
+        type: 'scatter'
+      },
+      title: {
+        text: '' // Title is handled by the component's template
+      },
+      xAxis: {
+        title: {
+          text: this.xField
+        }
+      },
+      yAxis: {
+        title: {
+          text: this.yField
+        }
+      },
+      series: [{
+        type: 'scatter',
+        name: `${this.yField} vs ${this.xField}`,
+        data: seriesData,
+        showInLegend: false,
+        point: {
+            events: {
+                click: (event: any) => {
+                    this.dispatchEvent(new CustomEvent('oneviz-scatter-click', {
+                        detail: {
+                            x: event.point.x,
+                            y: event.point.y,
+                            originalEvent: event
+                        },
+                        bubbles: true,
+                        composed: true
+                    }));
+                }
+            }
+        }
+      }]
+    };
+  }
+}
